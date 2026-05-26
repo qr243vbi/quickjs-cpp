5c6017a904265e06328f73e0eaf33ff51e7cebbf
qr243vbi <qr243vbi@atomicmail.io>
2026-05-26 20:31:18 +0800

    sync file quickjs.hpp

--- quickjs.hpp
+++ quickjs.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <map>
+#include <memory>
 #include <quickjs.h>
 #include <stdexcept>
 #include <vector>
@@ -13,40 +14,58 @@ class Array {
   public:
   virtual Value get(size_t index) const = 0;
   virtual bool set(size_t index, Value value) = 0;
-  virtual JSContext * context() const = 0;
   virtual size_t size() const = 0;
   template <typename T> bool set(size_t index, T value);
+  virtual ~Array() = default;
+  protected:
+  virtual JSContext * context() const = 0;
+  
 };
 
 class Object {
   public:
   virtual qjs::Value get(const std::string & name) const = 0;
-  virtual bool set(std::string index, qjs::Value value) = 0;
-  virtual JSContext * context() const = 0;
+  virtual bool set(const std::string& index, qjs::Value value) = 0;
+  virtual bool drop(const std::string& index) = 0;
   virtual size_t size() const = 0;
-  template <typename T> bool set(std::string index, T value);
-};
-
-struct Callback {
-    std::function<Value(Array*)> fn;
+  template <typename T> bool set(const std::string& index, T value);
+  virtual ~Object() = default;
+  protected:
+  virtual JSContext * context() const = 0;
 };
 
 class Function {
-  virtual qjs::Value call(Array*) = 0;
+  public:
+  virtual qjs::Value call(const Value&, Array*) = 0;
+  virtual ~Function() = default;
 };
 
-static JSValue js_trampoline(JSContext *ctx, JSValueConst this_val,
+struct Callback: public Function {
+  std::function<Value(const Value&, Array*)> fn;
+  virtual qjs::Value call(const Value& val, Array*arr) override ;
+};
+
+static JSValue js_function_trampoline(JSContext *ctx, JSValueConst this_val,
                              int argc, JSValueConst *argv,
                              int magic, JSValueConst *data);
 
 class Value : public Array, public Object, public Function{
 public:
-  virtual Value call(Array * array) override {
+  virtual bool drop(const std::string & val) override {
+    if (isObject()){
+      auto ctx = context();
+      if (JS_DeleteProperty(ctx, raw(), JS_NewAtomLen(ctx, val.c_str(), val.size()), 0) == true){
+        return true;
+      };
+    }
+    return false;
+  }
+  virtual Value call(const Value& val, Array * array) override {
+    auto ctx = context();
     if (!isFunction()){
-      return Value(nullptr, JS_UNDEFINED, true);
+      return Value(ctx, JS_UNDEFINED, true);
     }
     auto func = raw();
-    auto ctx = context();
     auto size = array->size();
     JSValueConst * argv = new JSValueConst[size];
 
@@ -54,7 +73,7 @@ public:
       argv[i] = array->get(i).raw();
     }
 
-    JSValue result = JS_Call(ctx, func, JS_UNDEFINED, array->size(), argv);
+    JSValue result = JS_Call(ctx, func, val.raw(), array->size(), argv);
     delete [] argv;
     return Value(ctx, result, true);
   }
@@ -164,12 +183,6 @@ public:
 
   Value(Value &&val) noexcept { ASSIGN_MOVE(val); }
 
-  virtual JSContext *context() const override { return ctx_; }
-
-  JSValue raw() const { return value_; }
-
-  JSValue rawdup() const { return JS_DupValue(ctx_, value_); }
-
   virtual bool set(size_t index, qjs::Value value) override {
     if (isArray()){
         auto ctx = context();
@@ -181,7 +194,7 @@ public:
     }
   }
 
-  virtual bool set(std::string index, qjs::Value value) override {
+  virtual bool set(const std::string& index, qjs::Value value) override {
     if (isObject()){
         JS_SetPropertyStr(context(), raw(), index.c_str(), value.rawdup());
         return true;
@@ -191,12 +204,12 @@ public:
   }
 
   virtual qjs::Value get(size_t index) const override {
+    auto ctx = context();
     if (isArray()){
-        auto ctx = context();
         auto new_val = JS_GetPropertyUint32(ctx, raw(), index);
         return Value(ctx, new_val, true);
     } else {
-        return Value(nullptr, JS_UNDEFINED, true);
+        return Value(ctx, JS_UNDEFINED, true);
     }
   }
 
@@ -210,12 +223,12 @@ public:
   }
 
   virtual qjs::Value get(const std::string & name) const override {
+    auto ctx = context();
     if (isObject()){
-        auto ctx = context();
         auto new_val = JS_GetPropertyStr(ctx, raw(), name.c_str());
         return Value(ctx, new_val, true);
     } else {
-        return Value(nullptr, JS_UNDEFINED, true);
+        return Value(ctx, JS_UNDEFINED, true);
     }
   }
 
@@ -245,9 +258,19 @@ public:
 
   bool isBigInt() const { return JS_VALUE_GET_TAG(raw()) == JS_TAG_BIG_INT; }
 
-  ~Value() { free(); }
+  virtual ~Value() { free(); }
 
-private:
+  friend void throwIfException(JSContext *ctx, qjs::Value &value);
+  friend class PointerArray;
+  friend JSValue js_function_trampoline(JSContext *ctx, JSValueConst this_val,
+                             int argc, JSValueConst *argv,
+                             int magic, JSValueConst *data);
+protected:
+
+  JSValue rawdup() const { return JS_DupValue(ctx_, value_); }
+  JSValue raw() const { return value_; }
+
+  virtual JSContext *context() const override { return ctx_; }
   void free() {
     if (ctx_ != nullptr) {
       JS_FreeValue(ctx_, value_);
@@ -262,21 +285,26 @@ private:
     return set(index, qjs::Value(context(), value));
   }
 
-  template <typename T> bool Object::set(std::string index, T value){
+  template <typename T> bool Object::set(const std::string &index, T value){
     return set(index, qjs::Value(context(), value));
   }
 
+  qjs::Value Callback::call(const Value& val, Array*arr) { // NOLINT(misc-definitions-in-headers)
+    return fn(val, arr);
+  };
 
 class PointerArray: public Array {
   public:
+  virtual ~PointerArray(){};
   PointerArray(JSContext * ctx, int argc, JSValueConst *argv){
     
   }
   virtual Value get(size_t index) const override { 
+    auto ctx = context();
     if (index >= length){
-      return Value(nullptr, JS_UNDEFINED, true);
+      return Value(ctx, JS_UNDEFINED, true);
     } else {
-      return Value(context(), array[index], false);
+      return Value(ctx, array[index], false);
     }
   };
   virtual bool set(size_t index, Value value) override {
@@ -299,25 +327,68 @@ class PointerArray: public Array {
   JSContext * ctx;
 };
 
-JSValue js_trampoline(JSContext *ctx, JSValueConst this_val,
+struct QuickJS_CppClasses {
+  JSClassID function_class;
+};
+
+static void function_finalizer(JSRuntime *rt, JSValue val);
+
+static JSClassDef function_def = {
+   "qr243vbi_function",
+   function_finalizer
+};
+
+static QuickJS_CppClasses * getClasses (JSRuntime * rt) {
+  auto cls = (QuickJS_CppClasses *) JS_GetRuntimeOpaque(rt);
+  if (cls == nullptr){
+    cls = new QuickJS_CppClasses();
+    JS_NewClassID(rt, &cls->function_class);
+    JS_NewClass(rt, cls->function_class, &function_def);
+    JS_SetRuntimeOpaque(rt, cls);
+  }
+  return cls;
+}
+
+static QuickJS_CppClasses * getClasses (JSContext * rt) {
+  return getClasses(JS_GetRuntime(rt));
+}
+
+JSValue js_function_trampoline(JSContext *ctx, JSValueConst this_val, // NOLINT(misc-definitions-in-headers)
                              int argc, JSValueConst *argv,
-                             int magic, JSValueConst *data) {
-    Callback *cb = (Callback *)JS_VALUE_GET_PTR(data[0]);
-    auto ptr = new PointerArray(ctx, argc, argv);
-    return cb->fn(ptr).rawdup();
+                             int magic, JSValueConst *data) { 
+    JSClassID class_id = getClasses(ctx)->function_class;
+    qjs::Function *cb = (qjs::Function*)JS_GetOpaque(data[0], class_id);
+    std::shared_ptr<PointerArray> ptr = std::make_shared<PointerArray>(ctx, argc, argv);
+    return cb->call(Value(ctx, this_val, false), ptr.get()).rawdup();
+}
+
+void function_finalizer(JSRuntime *rt, JSValue val) {
+  QuickJS_CppClasses * cls = (QuickJS_CppClasses *)JS_GetRuntimeOpaque(rt);
+  if (cls != nullptr){
+    Function * opaque = (Function *)JS_GetOpaque(val, cls->function_class);
+    if (opaque != nullptr) {
+      delete opaque;
+    }
+  }
 }
 
 class Runtime {
 public:
   Runtime() {
     rt_ = JS_NewRuntime();
-    if (!rt_)
+    if (!rt_) {
       throw std::runtime_error("JS_NewRuntime failed");
+    }
   }
 
   ~Runtime() {
-    if (rt_)
+    if (rt_) {
+      void *data = JS_GetRuntimeOpaque(rt_);
+      if (data) {
+        free(data);
+      }
       JS_FreeRuntime(rt_);
+    }
   }
 
   Runtime(const Runtime &) = delete;
@@ -362,10 +433,20 @@ public:
   }
 
   ~Context() {
-    if (ctx_ != nullptr)
+    if (ctx_ != nullptr){
+      void *data = JS_GetContextOpaque(ctx_);
+      if (data) {
+        free(data);
+      }
       JS_FreeContext(ctx_);
-    if (rt_ != nullptr)
+    }
+    if (rt_ != nullptr){
+      void *data = JS_GetRuntimeOpaque(rt_);
+      if (data) {
+        free(data);
+      }
       JS_FreeRuntime(rt_);
+    }
   }
 
   Context(const Context &) = delete;
@@ -376,6 +457,71 @@ public:
   template<typename T> qjs::Value newValue(T t){
     return qjs::Value(ctx_, t);
   }
+
+  qjs::Value newFunction(qjs::Function * fn)
+  {
+    if (qjs::Value* d = dynamic_cast<qjs::Value*>(fn)) {
+      return *d;
+    }
+    JSContext *ctx = this->ctx_;
+    JSValue data_obj =
+        JS_NewObjectClass(ctx, getClasses(ctx)->function_class);
+
+    JS_SetOpaque(data_obj, fn);
+
+    JSValue data[1] = {
+        data_obj
+    };
+
+    JSValue func = JS_NewCFunctionData(
+        ctx,
+        js_function_trampoline,
+        0,      // length
+        0,      // magic
+        1,      // data count
+        data
+    );
+
+    JS_FreeValue(ctx, data_obj);
+
+    return Value(ctx, func, true);
+  }
+
+  qjs::Value newFunction(std::function<Value(const Value&, Array*)> fn){
+    auto call = new Callback();
+    call->fn = fn;
+    return newFunction(call);    
+  }
+
+  qjs::Value getGlobal() {
+    return qjs::Value(ctx_, JS_GetGlobalObject(ctx_), true);
+  }
+
+  qjs::Value getGlobal(const std::string & name){
+    auto global = getGlobal();
+    return global.get(name);
+  }
+
+
+  bool setGlobal(const std::string & name, const qjs::Value & val){
+    auto global = getGlobal();
+    return global.set(name, val);
+  };
+
+  bool dropGlobal(const std::string &name){
+    auto global = getGlobal();
+    return global.drop(name);
+  }
+
+
+  template <
+    typename T,
+    std::enable_if_t<!std::is_same_v<std::decay_t<T>, Value>, int> = 0,
+    std::enable_if_t<!std::is_same_v<std::decay_t<T>, JSValue>, int> = 0
+  >
+  bool setGlobal(const std::string & name, T val){
+    return setGlobal(name, qjs::Value(ctx_, val));
+  };
 
   qjs::Value newObject() {
     qjs::Value val;
